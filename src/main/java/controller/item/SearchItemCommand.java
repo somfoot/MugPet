a4eb036630266b241c39f38b2b42d35feca51535
smjun0526 <smjun0526@naver.com>
2023-05-05 21:24:24 +0900

    [ADD] 중고거래, 검색 command class

--- src/main/java/controller/item/SearchItemCommand.java
+++ src/main/java/controller/item/SearchItemCommand.java
@@ -1,5 +1,13 @@
 package controller.item;
 
 public class SearchItemCommand {
+	private String searchInput;
 
+	public String getSearchInput() {
+		return searchInput;
+	}
+
+	public void setSearchInput(String searchInput) {
+		this.searchInput = searchInput;
+	}
 }
