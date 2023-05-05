d57b1d62e89c87824f2fe078a7691a1948466db2
Hyouzl <hyouzl0521@gmail.com>
2023-05-05 21:19:43 +0900

    newAccount,newPetCommand

--- src/main/java/controller/member/NewPetCommand.java
+++ src/main/java/controller/member/NewPetCommand.java
@@ -1,5 +1,42 @@
 package controller.member;
 
 public class NewPetCommand {
+	
+	private String name;
+	private String species;
+	private String birth;
+	
+	
+	
+	
+	public NewPetCommand(String name, String species, String birth) {
+		super();
+		this.name = name;
+		this.species = species;
+		this.birth = birth;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSpecies() {
+		return species;
+	}
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	
+	
+	
 
 }
