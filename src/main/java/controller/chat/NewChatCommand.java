a4069d535ab597c91ae501c6f322dae8bb4e87bf
MinJeong-Jang <zena4649@gmail.com>
2023-05-05 14:45:04 +0900

    [ADD] Controller&Command_민정

--- src/main/java/controller/chat/NewChatCommand.java
+++ src/main/java/controller/chat/NewChatCommand.java
@@ -1,5 +1,75 @@
 package controller.chat;
 
-public class NewChatCommand {
+import java.util.Date;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class NewChatCommand {
+	@NotEmpty
+	private int chat_id;
+	
+	@NotEmpty
+	private int sender_id;
+	
+	@NotEmpty
+	private int receiver_id;
+	
+	@NotEmpty
+	private String message;
+	
+	@NotEmpty
+	private Date chatDt;
+	
+	public NewChatCommand() {
+		super();
+	}
+	
+	public NewChatCommand(int chat_id, int sender_id, int receiver_id, String message, Date chatDt) {
+		super();
+		this.chat_id = chat_id;
+		this.sender_id = sender_id;
+		this.receiver_id = receiver_id;
+		this.message = message;
+		this.chatDt = chatDt;
+	}
+
+	public int getChat_id() {
+		return chat_id;
+	}
+
+	public void setChat_id(int chat_id) {
+		this.chat_id = chat_id;
+	}
+
+	public int getSender_id() {
+		return sender_id;
+	}
+
+	public void setSender_id(int sender_id) {
+		this.sender_id = sender_id;
+	}
+
+	public int getReceiver_id() {
+		return receiver_id;
+	}
+
+	public void setReceiver_id(int receiver_id) {
+		this.receiver_id = receiver_id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Date getChatDt() {
+		return chatDt;
+	}
+
+	public void setChatDt(Date chatDt) {
+		this.chatDt = chatDt;
+	}
 }
--- src/main/java/controller/community/CommunityController.java
+++ src/main/java/controller/community/CommunityController.java
@@ -1,5 +1,62 @@
 package controller.community;
 
-public class CommunityController {
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("~~~~")
+public class CommunityController {
+	/*진행 상황에 따라 코드 수정 必*/
+	
+	@RequestMapping("필요하다면 사용")
+	public String viewCommunity() {
+		//게시글 상세보기
+		
+		return "~~~~";
+	}
+	
+	@RequestMapping("필요하다면 사용")
+	public String commuityList() {
+		//게시글 목록 보기
+		
+		return "~~~~";
+	}
+	
+	@RequestMapping("필요하다면 사용")
+	public String deleteCommunity(int com_id) {
+		//게시글 삭제
+		
+		return "~~~~";
+	}
+	
+	//글 수정 버튼 클릭 시, 수정 폼으로 이동
+	@RequestMapping(method = RequestMethod.GET)
+	public String updateForm(int com_id) {
+		
+		return "~~~~";
+	}
+	
+	//수정 폼에서 수정 완료 버튼 클릭 시, 해당 글과 관련된 내용을 db에 수정해 저장 후 목록or상세보기 페이지로 이동
+	@RequestMapping(method = RequestMethod.POST)
+	public String updateSubmit() {
+		
+		return "~~~~";
+	}
+	
+	//글 작성 버튼 누르면 폼으로 이동
+	@RequestMapping(method = RequestMethod.GET)
+	public String form() {
+		
+		return "~~~~";
+	}
+	
+	//폼에서 작성 완료 버튼을 누르면 해당 글과 관련된 내용을 db에 저장 후 목록or상세보기 페이지로 이동
+	@RequestMapping(method = RequestMethod.POST)
+	public String submit(NewCommunityCommand comCommand) {
+		
+		return "~~~~";
+	}
 }
